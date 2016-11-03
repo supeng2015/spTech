11ad8079bef86c24e4b3640785c00b46c3a0d705
sp <281317914@qq.com>
2016-11-03 09:29:48 +0800

    upload

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,18 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'SP的技术文档' });
 });
+router.post('/myupload', function(req, res, next) {
+
+  console.log(req.body);
+  res.json({
+    text : '上传成功'	
+  })
+});
+router.get('/myupload', function(req, res, next) {
+  console.log(req);
+  res.json({
+    text : '上传成功'	
+  })
+});
 
 module.exports = router;
